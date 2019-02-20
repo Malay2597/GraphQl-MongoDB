a31c86fa3fdac1553682225d8adf6dc66564c357
malay <Malay.Shah@bravissimo.com>
2019-02-20 14:46:40 +0530

    added bookings

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -2,6 +2,14 @@ const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
 
+    type Booking{
+        _id:ID!
+        event:Event!
+        user :User!
+        createdAt:String!
+        UpdatedAt:String!
+    }
+
     type User {
         _id:ID!
         email:String!
@@ -32,12 +40,15 @@ module.exports = buildSchema(`
 
     type RootQuery {
         events : [Event!]!
+        bookings : [Booking!]!
     }
 
     
     type RootMutation {
         createEvent(eventInput:EventInput):Event
         createUser(userInput:UserInput):User
+        bookEvent(eventId:ID!):Booking!
+        cancelBooking(bookingId:ID!):Event!
     }
 
     schema {
